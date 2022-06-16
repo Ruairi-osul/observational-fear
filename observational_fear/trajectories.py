0eba5ead87fe30dacc9a981ef7838dbfed47e5db
Ruairi-osul <rorylikesmusic@gmail.com>
2022-06-16 09:43:58 -0400

    UMAP

--- observational_fear/trajectories.py
+++ observational_fear/trajectories.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.decomposition import PCA, KernelPCA
+from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
 from pathlib import Path
